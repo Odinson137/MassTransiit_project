3cf44a4b3f95c3d1aac79e4c8bc1c3aa5a45ade0
Yura buryy <buryy137@gmail.com>
2024-02-06 21:36:37 +0300

    Добавьте файлы проекта.

---
+++ MassTransient_client_project/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace MassTransitExample
+{
+    public interface IMessage
+    {
+        string Text { get; }
+    }
+
+    public class MessageConsumer : IConsumer<IMessage>
+    {
+        public async Task Consume(ConsumeContext<IMessage> context)
+        {
+            await Console.Out.WriteLineAsync($"Получено сообщение: {context.Message.Text}");
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint("message_queue", ep =>
+                {
+                    ep.Consumer<MessageConsumer>();
+                });
+            });
+
+            await busControl.StartAsync(); 
+
+            Console.WriteLine("Ожидание сообщений. Нажмите любую клавишу для завершения.");
+            Console.ReadKey();
+
+            await busControl.StopAsync(); 
+        }
+    }
+}
---
+++ MassTransiit_project/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace MassTransitExample;
+
+public interface IMessage
+{
+    string Text { get; }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+            cfg.Host(new Uri("rabbitmq://localhost/"), h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+        });
+
+        await busControl.StartAsync(); // Запускаем шину сообщений
+
+        try
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "q")
+                {
+                    break;
+                }
+
+                var endpoint = await busControl.GetSendEndpoint(new Uri("rabbitmq://localhost/message_queue"));
+                await endpoint.Send<IMessage>(new { Text = str });
+            }
+
+        }
+        finally
+        {
+            await busControl.StopAsync(); // Останавливаем шину сообщений
+        }
+    }
+}
